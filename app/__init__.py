d8b22206e7dadf01a9a2fa434365f56db96e2b97
atifar <tyatya68@gmail.com>
2018-08-16 22:48:03 -0700

    Docker compose runs app and MongoDB containers. Add root API endpoint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,35 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
 
 from config import Config
 
 
 def create_app(config_class=Config):
+    # create and configure the application
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # connect to the database server
+    client = MongoClient(
+        host=app.config['MONGO_HOST'],
+        port=app.config['MONGO_PORT']
+    )
+
+    # create the database
+    db = client[app.config['MONGO_DBNAME']]
 
     # sanity check
     @app.route('/sanity')
     def sanity_check():
         return 'Sanity check passed.'
+
+    @app.route('/', methods=['GET'])
+    def home():
+        data = {'keys_url': request.url_root + 'keys'}
+        resp = jsonify(data)
+        resp.status_code = 200
+        return resp
 
     return app
--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'sekrit-o-flask')
+    MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'kv_db')
+    MONGO_HOST = os.environ.get('MONGO_HOST', 'mongodb://localhost')
+    MONGO_PORT = int(os.environ.get('MONGO_PORT', 27017))
